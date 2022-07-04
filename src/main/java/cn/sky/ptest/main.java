a8afef324d5dd425a0c5f075b263e58abac82c96
Layne <Layne@atguigu.com>
2022-07-04 17:32:57 +0800

    ptest

---
+++ src/main/java/cn/sky/ptest/main.java
@@ -0,0 +1,13 @@
+package cn.sky.ptest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class main {
+    public static void main(String[] args) {
+
+        List<String> list = new ArrayList<String>();
+        List<Integer> dw1 = new ArrayList<>();
+        System.out.println(12);
+    }
+}
